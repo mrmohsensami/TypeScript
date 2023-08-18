dc8d008edc56e85e7ac2069148fd3d032509a1e0
MohsenSami <mohsensami88@gmail.com>
2023-08-18 17:51:46 +0330

    type basics

--- sandbox.ts
+++ sandbox.ts
@@ -1,3 +1,14 @@
-const input = document.querySelector('#search');
+// 1.Compile
+// const input = document.querySelector('#search');
+// console.log(input);
 
-console.log(input);
+// 2.Type Basics
+//  (Implicit Type )
+let fname = 'John';
+let age = 34;
+let isMale = true;
+
+const circ = (diameter: number) => {
+    return diameter * Math.PI;
+};
+console.log(circ(10));
