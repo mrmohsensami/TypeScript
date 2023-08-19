7d91297a56f6c49804705a2af2b338e63a8e7d63
MohsenSami <mohsensami88@gmail.com>
2023-08-19 20:36:35 +0330

    3-Array and Object implicit type

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,28 @@
-var input = document.querySelector('#search');
-console.log(input);
+// 1.Compile
+// const input = document.querySelector('#search');
+// console.log(input);
+// 2.Type Basics
+//  (Implicit Type )
+// let fname = 'John';
+// let age = 34;
+// let isMale = true;
+// const circ = (diameter: number) => {
+//     return diameter * Math.PI;
+// };
+// console.log(circ(10));
+// 3. Objects and Array
+// let names = ['John', 'Peter', 'Mary'];
+// names = 'alex'; // Error
+// names[1] = 24; // Error
+// names.push('Alex'); // Ok
+// names[1] = 'Bob'; // Ok
+// let mixed = ['John', 34];
+// mixed[1] = 'Bob'; // Ok
+// mixed.push('Kevin'); // Ok
+// let person = {
+//     name: 'John',
+//     age: 34,
+//     isMale: true,
+// };
+// person.email = 'email@example.com'; // Error
+// person.name = 10; // Error
--- sandbox.ts
+++ sandbox.ts
@@ -4,11 +4,27 @@
 
 // 2.Type Basics
 //  (Implicit Type )
-let fname = 'John';
-let age = 34;
-let isMale = true;
+// let fname = 'John';
+// let age = 34;
+// let isMale = true;
+// const circ = (diameter: number) => {
+//     return diameter * Math.PI;
+// };
+// console.log(circ(10));
 
-const circ = (diameter: number) => {
-    return diameter * Math.PI;
-};
-console.log(circ(10));
+// 3. Objects and Array
+// let names = ['John', 'Peter', 'Mary'];
+// names = 'alex'; // Error
+// names[1] = 24; // Error
+// names.push('Alex'); // Ok
+// names[1] = 'Bob'; // Ok
+// let mixed = ['John', 34];
+// mixed[1] = 'Bob'; // Ok
+// mixed.push('Kevin'); // Ok
+// let person = {
+//     name: 'John',
+//     age: 34,
+//     isMale: true,
+// };
+// person.email = 'email@example.com'; // Error
+// person.name = 10; // Error
