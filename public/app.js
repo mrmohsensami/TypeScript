4126189f8900030cf78e2609922843371cf3f29e
MohsenSami <mohsensami88@gmail.com>
2023-08-22 21:55:52 +0330

    classes

--- public/app.js
+++ public/app.js
@@ -1,18 +1,29 @@
 "use strict";
-var anchor = document.querySelector('a');
-// // if(anchor){
-// //     console.log(anchor.href)
-// // }
-// console.log(anchor.href);
-// const form = document.querySelector('form')!;
-// console.log(form.children);
-var form = document.querySelector('.new-item-form');
-//inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
-    e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
-});
+// 10. Type Casting
+// const anchor = document.querySelector('a')!;
+// const form = document.querySelector('.new-item-form') as HTMLFormElement;
+// //inputs
+// const type = document.querySelector('#type') as HTMLSelectElement;
+// const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+// const details = document.querySelector('#details') as HTMLInputElement;
+// const amount = document.querySelector('#amount') as HTMLInputElement;
+// 11. classes
+var Invoice = /** @class */ (function () {
+    function Invoice(c, d, a) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    Invoice.prototype.format = function () {
+        return "".concat(this.client, " owes ").concat(this.amount, " for ").concat(this.details);
+    };
+    return Invoice;
+}());
+var invOne = new Invoice('Client 1', 'Details 1', 250);
+var invTwo = new Invoice('Client 2', 'Details 2', 400);
+console.log(invOne.format());
+console.log(invTwo.format());
+var invoices = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
--- src/app.ts
+++ src/app.ts
@@ -6,3 +6,27 @@
 // const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 // const details = document.querySelector('#details') as HTMLInputElement;
 // const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// 11. classes
+class Invoice {
+    client: string;
+    details: string;
+    amount: number;
+
+    constructor(c: string, d: string, a: number) {
+        this.client = c;
+        this.details = d;
+        this.amount = a;
+    }
+    format() {
+        return `${this.client} owes ${this.amount} for ${this.details}`;
+    }
+}
+const invOne = new Invoice('Client 1', 'Details 1', 250);
+const invTwo = new Invoice('Client 2', 'Details 2', 400);
+console.log(invOne.format());
+console.log(invTwo.format());
+let invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+console.log(invoices);
